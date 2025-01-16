38a4812fdc6c5e0fa972cc99755789a18cbec041
Shagun Bandi <shagun.bandi@fareharbor.com>
2025-01-16 21:47:05 +0100

    Image generator

---
+++ services/image_generator.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from openai import OpenAI
+
+
+load_dotenv()
+model = ChatOpenAI(model="gpt-4o")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def generate_image_from_summary(summarized_news: str):
+    response = client.images.generate(
+        prompt=f"Generate an image that summarises the article into image: ARTICLE \n\n {summarized_news}.",
+        n=3,
+        size="512x512",
+    )
+
+    image_urls = [item.url for item in response.data]
+
+    return image_urls
